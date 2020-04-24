e4e99bd6989f73a4c7fb5fc610a638647d686b0f
Justin Kaseman <justinkaseman@live.com>
2020-04-23 17:21:17 -0700

    Promisify contract deployment

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,7 +2,8 @@
 var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+module.exports = function (deployer) {
+  deployer
+    .deploy(SquareVerifier)
+    .then(() => deployer.deploy(SolnSquareVerifier, SquareVerifier.address));
 };
